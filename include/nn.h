7fea3abbd7361f03827551405938ef8cb5084275
toxypiks <HannaHinkelbein89@gmx.de>
2026-03-16 19:04:10 +0100

    Implement header for matrix operations

---
+++ include/nn.h
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+#ifndef NN_H
+#define NN_H
+
+typedef struct {
+    size_t rows;
+    size_t cols;
+    float *es; // points to start value of matrix
+} Mat;
+
+Mat mat_alloc(size_t rows, size_t cols);
+void mat_dot(Mat dst, Mat a, Mat b);
+void mat_sum(Mat dst, Mat a);
+void mat_print();
+
+#endif // NN_H
---
+++ src/nn.c
@@ -0,0 +1,25 @@
+#include "nn.h"
+#include <assert.h>
+
+Mat mat_alloc(size_t rows, size_t cols)
+{
+    Mat m;
+    m.rows = rows;
+    m.cols = cols;
+    m.es = malloc(sizeof(*m.es)*rows*cols);
+    assert(m.es != NULL);
+    return m;
+}
+
+void mat_dot(Mat dst, Mat a, Mat b)
+{
+    (void) dst;
+    (void) a;
+    (void) b;
+}
+void mat_sum(Mat dst, Mat a)
+{
+    (void) dst;
+    (void) a;
+}
+void mat_print(void);
